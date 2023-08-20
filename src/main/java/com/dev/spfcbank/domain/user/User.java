ac0a58d283af945b75e13cf984b23f2e33eddfd1
Matheus Bassi <bassi.matheus@outlook.com>
2023-08-20 11:37:32 -0300

    feat: created user entity

---
+++ src/main/java/com/dev/spfcbank/domain/user/User.java
@@ -0,0 +1,33 @@
+package com.dev.spfcbank.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String senha;
+    private BigDecimal balance;
+    private UserType userType;
+
+}
