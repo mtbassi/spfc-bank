d4bbdaacfd9e3d118cfbb83b728452450fb635d7
Matheus Bassi <bassi.matheus@outlook.com>
2023-08-20 00:42:13 -0300

    feat: initial config

---
+++ src/main/java/com/dev/spfcbank/SpfcBankApplication.java
@@ -0,0 +1,13 @@
+package com.dev.spfcbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpfcBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpfcBankApplication.class, args);
+    }
+
+}
