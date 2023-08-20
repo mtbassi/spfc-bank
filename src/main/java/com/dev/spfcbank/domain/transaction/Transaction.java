fb12065c0dccbc6a416ee3ec4e6b1ed8b9bec985
Matheus Bassi <bassi.matheus@outlook.com>
2023-08-20 15:52:35 -0300

    feat: created constructor that receives dto

--- src/main/java/com/dev/spfcbank/domain/transaction/Transaction.java
+++ src/main/java/com/dev/spfcbank/domain/transaction/Transaction.java
@@ -2,12 +2,20 @@ package com.dev.spfcbank.domain.transaction;
 
 import com.dev.spfcbank.domain.user.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity(name = "transactions")
 @Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction {
 
     @Id
@@ -21,4 +29,9 @@ public class Transaction {
     @JoinColumn(name = "receiver_id/")
     private User receiver;
 
+    public Transaction(TransactionDTO data) {
+        this.sender = data.sender();
+        this.amount = data.value();
+        this.receiver = data.receiver();
+    }
 }
