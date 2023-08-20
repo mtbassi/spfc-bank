315b4e2053bed5d0bb1b2f5ea1e06220c0a3fdcc
Matheus Bassi <bassi.matheus@outlook.com>
2023-08-20 11:56:11 -0300

    feat: created user service

--- src/main/java/com/dev/spfcbank/service/UserService.java
+++ src/main/java/com/dev/spfcbank/service/UserService.java
@@ -1,27 +1,21 @@
 package com.dev.spfcbank.service;
 
 import com.dev.spfcbank.domain.user.User;
+import com.dev.spfcbank.domain.user.UserDTO;
 import com.dev.spfcbank.domain.user.UserType;
 import com.dev.spfcbank.repository.UserRepository;
-import jakarta.transaction.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository repository;
-
-    public Transaction createTransaction(User sender, User receiver, BigDecimal amount) throws Exception {
-
-        this.validateTransaction(sender, amount);
-
-
-        return null;
-    }
 
     public void validateTransaction(User sender, BigDecimal amount) throws Exception {
         if (sender.getUserType().equals(UserType.MERCHANT)){
@@ -30,5 +24,17 @@ public class UserService {
         if (sender.getBalance().compareTo(amount) > 0){
             throw new Exception("Insufficient balance");
         }
+    }
+
+    public User findUserById(UUID id) throws Exception {
+        return this.repository.findById(id).orElseThrow(() -> new Exception("User not found"));
+    }
+
+    public List<User> findAllUsers(){
+        return this.repository.findAll();
+    }
+
+    public User createUser(UserDTO data){
+        return this.repository.save(new User(data));
     }
 }
