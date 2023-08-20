18e2b9e28698d518b241c89636182d90a69c349f
Matheus Bassi <bassi.matheus@outlook.com>
2023-08-20 15:51:40 -0300

    feat: created transaction dto

---
+++ src/main/java/com/dev/spfcbank/domain/transaction/TransactionDTO.java
@@ -0,0 +1,11 @@
+package com.dev.spfcbank.domain.transaction;
+
+import com.dev.spfcbank.domain.user.User;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record TransactionDTO(BigDecimal value,
+                             User sender,
+                             User receiver) {
+}
